837eebda1b5f084bed831463310fd4e9fe9b3e4d
VigneshM454 <120500685+VigneshM454@users.noreply.github.com>
2023-07-17 22:44:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,81 +1,89 @@
-const num=document.getElementsByClassName('nh')[0];
-const btn=document.getElementById('btn');
-const unread=document.getElementsByClassName('unread');
-const box1=document.getElementById('box');
-const box=document.getElementsByClassName('box');
-const dot=document.getElementsByClassName('dot');
-const option=document.getElementsByClassName('option')[0];
-const ip=document.getElementById('ip');
-const read=document.getElementById('read');
-const delet=document.getElementById('delete');
-const reply=document.getElementById('reply');
-let close=document.getElementById('close');
-let mybox =document.getElementById('mybox');
-const send=document.getElementById('send')
-let j=0;
-for (let i = 0; i < unread.length; i++) {
-    //const element = array[i];
-    j++;
-}
-//console.log(j)
-num.innerHTML=j;
-console.log(unread.length)
-btn.addEventListener('click',()=>{
-   // alert('hi');
-   for (let i = 0; i < box.length; i++) {
-    //const element = array[i];
-        if(box[i].classList.contains('unread')){
-            box[i].classList.remove('unread');
-            console.log('hi')
-            dot[i].style.display='none';
+const cname=document.getElementById('name');
+const cnum=document.getElementById('num');
+const cvc=document.getElementById('cvc');
+const expy=document.getElementById('expyear');
+const expm=document.getElementById('expmonth');
 
-        }
+const cardnum=document.getElementById('cardnum');
+const cardname=document.getElementById('cardname');
+const expdate1=document.getElementById('cardexpdate1');
+const expdate2=document.getElementById('cardexpdate2');
+const ccvc=document.getElementById('cardcvc');
+const btn=document.getElementById('verify');
+const form1=document.getElementsByTagName('form')[0];
+const namerr=document.getElementsByClassName('.namerr')[0];
+const numerr=document.getElementsByClassName('.numerr')[0];
+const daterr=document.getElementsByClassName('.dateerr')[0];
+const cverr=document.getElementsByClassName('.cverr')[0];
+const success=document.getElementById('success');
+let inputs=document.querySelectorAll('.inputs');
+const err=document.querySelectorAll('.err');
+let iparr=[cname,cnum,expy,expm,cvc];
+let oparr=[cardname,cardnum,expdate1,expdate2,ccvc];
+let j=(g)=>{
+  if(g<=1){
+    return g;
+  }
+  else if(g===2||g===3){
+    return 2;
+  }
+  else{
+    return 3;
+  }
+}
+let hasnumber=(str)=>{
+  return /\d/.test(str);
+}
+
+for(let i=0;i<5;i++){
+    iparr[i].oninput=function updatediv(){
+        let va=iparr[i].value;
+        oparr[i].textContent=va;
     }
-     j=0;
-    num.innerHTML=j;
-})
-
-for(let i=0;i<box.length; i++){
-    box[i].addEventListener('click',()=>{
-       // box[i].classList.add('option');
-       option.style.display='block';
-       option.style.cursor='pointer';
-       read.onclick=function(){
-            box[i].classList.remove('unread');
-            dot[i].style.display='none';
-        }
-        delet.onclick=function(){
-           // box[i].style.display='none';
-          box[i].remove();
-        }
-        reply.onclick=function(){
-            mybox.style.display='block';
-            
-            send.onclick=function(){
-              let input=ip.value;
-              if(input===''){
-                alert('cant send empty message')
-              }
-              
-              else{
-                console.log(input);
-                const newdiv=document.createElement('div');
-                const newcont=document.createTextNode(input);
-                newdiv.appendChild(newcont);
-                const newimg=document.createElement('img');
-                newimg.src='circle-user-solid.svg';
-                newimg.style.cssText='height:50px; width:50px;';
-                newdiv.appendChild(newimg);
-                newdiv.classList.add('myrep');
-                box[i].parentNode.insertBefore(newdiv,box[i+1]);
-              }
-            }
-
-            
-        }
-          //    read.onclick=box[i].classList.remove('unread');         -> it returns undecided things dont use it
-       close.addEventListener('click',()=>{
-            option.style.display='none';
-        })
-    })
 }
+
+let v=0;
+btn.addEventListener('click',()=>{
+  event.preventDefault();
+  for (let i = 0; i < 5; i++) {
+    if(iparr[i].value!==''){
+     inputs[i].style.border='1px solid black';
+      err[j(i)].style.display='none';
+      if(hasnumber(cname.value)){
+        err[0].style.display='flex'
+        err[0].style.display = 'flex';
+        err[0].innerHTML = 'Invalid input';
+        inputs[0].style.border='1px solid red';
+      }
+      if (expm.value < 1 || expm.value > 12 || expy.value < 23 || expy.value > 45) {
+        err[2].style.display = 'flex';
+        err[2].innerHTML = 'Invalid input';
+        inputs[2].style.border='1px solid red';
+        inputs[3].style.border='1px solid red';
+      }
+      if(cnum.value.length<8||cnum.value.lenght>18){
+        err[1].style.display = 'flex';
+        err[1].innerHTML = 'Invalid input';
+        inputs[1].style.border='1px solid red';
+      }
+      if (cvc.value < 100 || cvc.value > 10000) {
+        err[3].style.display = 'flex';
+        err[3].innerHTML = 'Invalid input';
+        inputs[4].style.border='1px solid red';
+      }
+      else  if(!(hasnumber(cname.value)||expm.value < 1 || expm.value > 12 || expy.value < 23 || expy.value > 45||cnum.value.length<8||cnum.value.lenght>18||cvc.value < 100 || cvc.value > 10000)){
+      setTimeout(() => {
+        form1.style.display = 'none';
+        success.style.display = 'flex';
+      }, 2000);
+      }
+    } 
+    else if (iparr[i].value==='') {
+      err[j(i)].style.display='flex'
+      inputs[i].style.border='1px solid red';
+      err[j(i)].innerHTML = "Can't be blank";
+    }
+  }
+  
+});
+        
